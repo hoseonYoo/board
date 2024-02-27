9229f4412aa8f0d9fd21105719713ff7b92fada0
dbghtjs112 <64840322+dbghtjs112@users.noreply.github.com>
2024-02-27 17:20:49 +0900

    update

--- src/main/java/com/mybatis/board/controller/BoardController.java
+++ src/main/java/com/mybatis/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.mybatis.board.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // 경로 앞단에 /boards 가 붙는 요청은 여기에
@@ -16,4 +17,19 @@ public class BoardController {
         log.info("*********BoardController GET /list");
         return "board/list";
     }
+
+    //게시글 등록 폼 요청
+    @GetMapping("/add")
+    public String addForm() {
+        log.info("*********BoardController GET boards/add");
+        return "board/add";
+    }
+
+    //게시글 등록 폼 처리
+    @PostMapping("/add")
+    public String addPro() {
+        log.info("*********BoardController POST boards/add");
+        return "redirect:/boards/list";
+    }
+
 }
